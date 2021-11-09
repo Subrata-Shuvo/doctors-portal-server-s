99c335ce7e854d94c7070da1641983cd009e60eb
unknown <himis946@gmail.com>
2021-11-09 11:27:56 +0600

    doctors db creating

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ require('dotenv').config();
 const port = process.env.PORT ||5000;
 
 app.use(cors());
+app.use(express.json());
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hvuud.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -13,7 +15,12 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
       try{
           await client.connect();
-          console.log('DB CONNECTED')
+          const database = client.db('doctors_portal');
+          const appointmentsCollection = database.collection('appointments');
+
+          app.post('/appointments', async(req, res) =>{
+            
+          })
       }
       finally{
        // await client.close();
@@ -41,11 +48,6 @@ run().catch(console.dir);
 
 
 
-
-
-
-
-
 app.get('/', (req, res) => {
   res.send('My Doctors Portal')
 })
@@ -53,3 +55,20 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at ${port}`)
 })
+
+
+
+
+
+/*
+app.get('/users')
+          app.post('/users')
+          app.get('/users/:id')
+          app.put('/users/:id')
+          app.delete('/users/:id')
+         
+         
+          // users: get
+          // users: post 
+          // users: put
+          // users: delete */
