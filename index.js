411981f0c0c67ea5fa33d88dcf515f719de001d0
unknown <himis946@gmail.com>
2021-11-09 13:35:48 +0600

    post

--- index.js
+++ index.js
@@ -19,8 +19,11 @@ async function run(){
           const appointmentsCollection = database.collection('appointments');
 
           app.post('/appointments', async(req, res) =>{
-            
-          })
+              const appointment = req.body;
+              const result = await appointmentsCollection.insertOne(appointment);
+              console.log(result);
+              res.send(result)
+          });
       }
       finally{
        // await client.close();
